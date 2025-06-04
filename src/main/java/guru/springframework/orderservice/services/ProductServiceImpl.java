8d266c211874731a4b426c1f5b341a79b1641e9f
Vincent Mos <vmos@qualogy.com>
2025-06-04 21:22:58 +0200

    added fix for pessimistic lock error

--- src/main/java/guru/springframework/orderservice/services/ProductServiceImpl.java
+++ src/main/java/guru/springframework/orderservice/services/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package guru.springframework.orderservice.services;
 import guru.springframework.orderservice.domain.Product;
 import guru.springframework.orderservice.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by jt on 6/21/22.
@@ -22,6 +23,7 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    @Transactional(readOnly = false)
     public Product updateQOH(Long id, Integer quantityOnHand) {
         Product product = productRepository.findById(id)
                 .orElseThrow();
